50e11d02e81c393dbcc5c2074f62268771dd0947
Parker Petersen <parkpetersen@gmail.com>
2018-04-11 23:06:06 -0600

    fix

--- scripts/client/game.js
+++ scripts/client/game.js
@@ -399,12 +399,12 @@ MyGame.main = (function(graphics, renderer, input, components) {
 
         let textureString = 'player-self-' + getTexture(playerSelf.model.direction, playerSelf.model.state);
         playerSelf.texture = MyGame.assets[textureString];
-        graphics.drawWorldBoundary(1, 1);
-        graphics.setCamera(playerSelf.model, 0, 5000, 0, 5000);
+        //graphics.drawWorldBoundary(1, 1);
+        graphics.setCamera(playerSelf.model, 0, 4800, 0, 4800);
         graphics.drawBackground();
         renderer.Player.render(playerSelf.model, playerSelf.texture);
         
-       // graphics.enableClipping(playerSelf.model, clip);
+       graphics.enableClipping(playerSelf.model, clip);
 
         for (let id in playerOthers) {
             let player = playerOthers[id];
@@ -419,7 +419,7 @@ MyGame.main = (function(graphics, renderer, input, components) {
             renderer.Missile.render(missiles[missile]);
         }
         
-       // graphics.disableClipping(clip);
+       graphics.disableClipping(clip);
         
         for (let id in explosions) {
             renderer.AnimatedSprite.render(explosions[id]);
